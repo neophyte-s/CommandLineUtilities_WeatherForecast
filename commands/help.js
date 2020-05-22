87cd99ac11d0824647ed15bab01512248f1ab41f
Swarnim <63869807+neophyte-s@users.noreply.github.com>
2020-05-22 22:49:53 +0530

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,9 @@
+// to export anything
+module.exports = function() {
+  console.log(` weather [command] <options>
+
+  today .............. show weather for today
+  forecast ........... show 5-day weather forecast
+  help ............... show help menu for a command`);
+};
+
